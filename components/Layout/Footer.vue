f1c81a783e57469492f312ac27f9ad25d853240c
menthe <adrien.le.mentheour@gmail.com>
2021-01-07 17:26:50 +0100

    👷‍♂️ footer links

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -6,10 +6,26 @@
                 <Social :content="data.social" />
             </div>
             <div class="wrapper-menu-legal">
-                <Logo />
+                <Logo class="logo-footer" />
                 <div class="wrapper-cols-footer">
                     <div v-for="(column, index) in data.linksColumn" :key="index" class="col-footer">
-                        <LinkTo v-for="(singleLink, linkIndex) in column.links" :key="linkIndex" :link="singleLink" />
+                        <LinkTo
+                            v-for="(singleLink, linkIndex) in column.links"
+                            :key="linkIndex"
+                            class="footer-link"
+                            :link="singleLink"
+                        />
+                    </div>
+                </div>
+                <div class="footer-bottom">
+                    <span v-if="data.smallText">{{ data.smallText }}</span>
+                    <div v-if="data.smallLinks" class="wrapper-small-links">
+                        <LinkTo
+                            v-for="(singleLink, linkIndex) in data.smallLinks"
+                            :key="linkIndex"
+                            class="footer-small-link"
+                            :link="singleLink"
+                        />
                     </div>
                 </div>
             </div>
@@ -52,5 +68,35 @@ export default {
 }
 .wrapper-menu-legal {
     padding: 50px 0 40px;
+}
+.logo-footer {
+    margin-bottom: 50px;
+}
+.wrapper-cols-footer {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-wrap: wrap;
+}
+.col-footer {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    margin-bottom: 30px;
+}
+.footer-link {
+    text-decoration: none;
+    margin-bottom: 10px;
+}
+.footer-bottom {
+    margin-top: 10px;
+    font-size: 1.3rem;
+    color: $neptune;
+}
+.wrapper-small-links {
+    margin-top: 10px;
+}
+.footer-small-link {
+    text-decoration: none;
 }
 </style>

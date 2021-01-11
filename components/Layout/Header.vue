ec09fa5bd0667812e3ba8066d8628301ab485a17
Clément Lemoine <clementlemoine.cl@gmail.com>
2021-01-11 18:10:56 +0100

    ✨ menu mobile

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -5,13 +5,13 @@
                 <Logo />
             </div>
             <div class="wrapper-burger content-pad">
-                <button class="burger" aria-label="Menu" @click="toggleMenuMobile">
+                <button class="burger" aria-label="Menu" :class="{ cross: showMenuMobile }" @click="toggleMenuMobile">
                     <span></span>
                     <span></span>
                     <span></span>
                 </button>
             </div>
-            <transition name="menu" :duration="2000">
+            <transition name="menu" :duration="{ enter: 900, leave: 600 }">
                 <div v-show="showMenuMobile" class="menu">
                     <div class="menu-inner">
                         <div class="menu-items">
@@ -120,12 +120,30 @@ export default {
         width: 30px;
         height: 1px;
         background-color: $white;
+        transform-origin: 100% 50%;
+        transition: 0.6s cubic-bezier(0.76, 0, 0.24, 1);
         &:nth-child(2) {
             top: 50%;
         }
         &:nth-child(3) {
             top: auto;
             bottom: 0;
+        }
+    }
+    &.cross {
+        span {
+            &:nth-child(1) {
+                transform: rotate(-45deg) scaleX(1.15) scaleY(1.15);
+                transition-delay: 0.15s;
+            }
+            &:nth-child(2) {
+                transform: scaleX(0);
+                opacity: 0;
+            }
+            &:nth-child(3) {
+                transform: rotate(45deg) scaleX(1.15) scaleY(1.15);
+                transition-delay: 0.15s;
+            }
         }
     }
 }
@@ -136,18 +154,37 @@ export default {
     top: 0;
     height: 100%;
     padding-top: 120px;
-    background-color: $dark;
     z-index: -1;
+    &::after {
+        content: '';
+        position: absolute;
+        top: 120px;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: $dark;
+        z-index: -1;
+        transform-origin: 50% 0%;
+    }
 }
 
 .menu-inner {
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     height: 100%;
     padding-bottom: 30px;
-    border-top: 1px solid $neptune;
     overflow-y: auto;
+    &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 1px;
+        background-color: $neptune;
+    }
 }
 
 .menu-items {
@@ -163,19 +200,37 @@ export default {
 
 .menu-enter,
 .menu-leave-to {
-    transform: translateY(-100%);
-    .menu-items,
+    &::after {
+        transform: scaleY(0);
+    }
+    .menu-item,
     .contact-link {
         opacity: 0;
         transform: translateY(-10px);
+    }
+    .menu-inner::after {
+        opacity: 0;
     }
 }
 
 //Open anim
 .menu-enter-active {
-    transition: transform 0.6s cubic-bezier(0.76, 0, 0.24, 1);
-    .menu-items {
-        transition: opacity 0.3s ease-in-out 0.4s, transform 0.3s ease-in-out 0.4s;
+    &::after {
+        transition: transform 0.8s cubic-bezier(0.76, 0, 0.24, 1);
+    }
+    .menu-inner::after {
+        transition: opacity 0.2s ease-in-out 0.4s;
+    }
+    .menu-item {
+        &:nth-child(1) {
+            transition: opacity 0.3s ease-in-out 0.4s, transform 0.3s ease-in-out 0.4s;
+        }
+        &:nth-child(2) {
+            transition: opacity 0.3s ease-in-out 0.45s, transform 0.3s ease-in-out 0.45s;
+        }
+        &:nth-child(3) {
+            transition: opacity 0.3s ease-in-out 0.5s, transform 0.3s ease-in-out 0.5s;
+        }
     }
     .contact-link {
         transition: opacity 0.3s ease-in-out 0.6s, transform 0.3s ease-in-out 0.6s;
@@ -184,12 +239,25 @@ export default {
 
 //Close anim
 .menu-leave-active {
-    transition: transform 0.6s cubic-bezier(0.76, 0, 0.24, 1) 0.15s;
-    .menu-items {
-        transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+    &::after {
+        transition: transform 0.6s cubic-bezier(0.76, 0, 0.24, 1);
+    }
+    .menu-inner::after {
+        transition: opacity 0.2s ease-in-out 0.2s;
+    }
+    .menu-item {
+        &:nth-child(1) {
+            transition: opacity 0.2s ease-in-out 0.2s, transform 0.2s ease-in-out 0.2s;
+        }
+        &:nth-child(2) {
+            transition: opacity 0.2s ease-in-out 0.15s, transform 0.2s ease-in-out 0.15s;
+        }
+        &:nth-child(3) {
+            transition: opacity 0.2s ease-in-out 0.1s, transform 0.2s ease-in-out 0.1s;
+        }
     }
     .contact-link {
-        transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+        transition: opacity 0.2s ease-in-out, transform 0.2s ease-in-out;
     }
 }
 </style>
--- components/Layout/Menu/MegaMenu.vue
+++ components/Layout/Menu/MegaMenu.vue
@@ -4,9 +4,31 @@
         <div ref="submenuWrapper" class="menu-item-content-wrapper">
             <div ref="submenu" class="menu-item-content">
                 <div class="submenu-links">
-                    <div v-for="link in data.megaMenu.links" :key="link.label" class="submenu-link">
-                        {{ link.label }}
-                    </div>
+                    <LinkTo v-for="link in data.megaMenu.links" :key="link.label" class="submenu-link" :link="link" />
+                </div>
+                <span class="submenu-title">{{ data.megaMenu.socialTitle }}</span>
+                <ul v-if="data.megaMenu.social" class="socials">
+                    <li v-for="link in data.megaMenu.social.socialLinks" :key="link.title" class="social">
+                        <a class="social-link" :class="link.iconName" :href="link.link" :aria-label="link.title"
+                            ><Icon :name="link.iconName"
+                        /></a>
+                    </li>
+                </ul>
+                <div class="menu-news">
+                    <span v-if="data.megaMenu.newsTitle" class="submenu-title">{{ data.megaMenu.newsTitle }}</span>
+                    <a :href="data.megaMenu.news.linkUrl" class="news-link">
+                        <img :src="data.megaMenu.news.cover.url" :alt="data.megaMenu.news.cover.alt" />
+                        <div class="news-info">
+                            <span class="news-date">{{
+                                new Intl.DateTimeFormat('en-US', {
+                                    month: 'short',
+                                    day: '2-digit'
+                                }).format(new Date(data.megaMenu.news.date))
+                            }}</span>
+                            <span>{{ data.megaMenu.news.readingTime }} min read</span>
+                        </div>
+                        <p class="news-title">{{ data.megaMenu.news.title }}</p>
+                    </a>
                 </div>
             </div>
         </div>
@@ -59,4 +81,80 @@ export default {
     }
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.submenu-links {
+    margin-bottom: 10px;
+}
+.socials {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 0;
+    margin: 0 0 48px;
+    > li {
+        flex: 0 0 auto;
+        margin-right: 40px;
+        &:last-child {
+            margin-right: 0;
+        }
+    }
+}
+
+.social-link {
+    display: block;
+    fill: $neptune;
+    &.linkedin {
+        width: 24px;
+        height: 27px;
+    }
+    &.twitter {
+        width: 28px;
+        height: 24px;
+    }
+    &.instagram {
+        width: 27px;
+        height: 27px;
+    }
+    &.medium {
+        width: 32px;
+        height: 18px;
+    }
+    > svg {
+        display: block;
+        width: 100%;
+        height: 100%;
+    }
+}
+
+.menu-news {
+    margin-bottom: 60px;
+}
+
+.news-link {
+    text-decoration: none;
+}
+
+.news-info {
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+    color: $neptune;
+    font-size: 1.4rem;
+}
+.news-date {
+    display: flex;
+    align-items: center;
+    &:after {
+        content: '';
+        width: 3px;
+        height: 3px;
+        margin: 0 10px;
+        border-radius: 50%;
+        background-color: currentColor;
+    }
+}
+.news-title {
+    margin: 15px 0 0;
+}
+</style>

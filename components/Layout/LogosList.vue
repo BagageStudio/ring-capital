c2d9e8a17ec3938c7285d727819fe4b8a635fd43
menthe <adrien.le.mentheour@gmail.com>
2021-01-12 11:53:33 +0100

    ✨ logos list

--- components/Layout/LogosList.vue
+++ components/Layout/LogosList.vue
@@ -1,7 +1,7 @@
 <template>
     <ul v-if="content" class="logos-list">
         <li v-for="logo in content" :key="logo.id">
-            <nuxt-link :to="logo.slug" :aria-label="logo.name" class="logo-btn">
+            <nuxt-link :to="logo.slug" :aria-label="logo.name" class="logo-link">
                 <img :src="logo.logo.url" :alt="logo.logo.alt" />
             </nuxt-link>
         </li>
@@ -18,4 +18,55 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logos-list {
+    display: flex;
+    flex-wrap: wrap;
+    width: calc(100% + 3px);
+    padding: 0 $gutter;
+    > li {
+        width: calc(#{percentage(1/2)} - 3px);
+        margin: 0 3px 3px 0;
+    }
+}
+.logo-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70px;
+    padding: 20px 15px;
+    background: $orbit;
+    &:hover,
+    &:focus {
+        > img {
+            opacity: 0.7;
+        }
+    }
+    > img {
+        max-width: 100%;
+        max-height: 100%;
+        transition: opacity 0.2s ease-out;
+    }
+}
+
+@media (min-width: $tablet) {
+    .logos-list {
+        > li {
+            width: calc(#{percentage(1/3)} - 3px);
+        }
+    }
+}
+@media (min-width: $desktop) {
+    .logo-link {
+        height: 100px;
+        padding: 35px 25px;
+    }
+}
+@media (min-width: $desktop-large) {
+    .logos-list {
+        > li {
+            width: calc(#{percentage(1/6)} - 3px);
+        }
+    }
+}
+</style>

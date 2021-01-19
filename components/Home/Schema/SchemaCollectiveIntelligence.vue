876d305dc1cddc6524dc355bba028d333a523e08
Clément Lemoine <clementlemoine.cl@gmail.com>
2021-01-19 15:24:57 +0100

    🚧 schema collective intelligence

---
+++ components/Home/Schema/SchemaCollectiveIntelligence.vue
@@ -0,0 +1,293 @@
+<template>
+    <div class="schema">
+        <svg viewBox="0 0 1442 1074" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="collectiveintelligence">
+                <rect width="1440" height="1073" transform="translate(1 1)" fill="white" />
+                <g id="mask_3">
+                    <mask
+                        id="maskCI0"
+                        mask-type="alpha"
+                        maskUnits="userSpaceOnUse"
+                        x="737"
+                        y="0"
+                        width="705"
+                        height="424"
+                    >
+                        <path
+                            :id="`model_${links[2].id}`"
+                            d="M1441 422.822C1261.02 304.779 1071.42 188.888 873.882 76.9214C828.328 51.1014 782.858 25.7772 737.5 0.954684"
+                            stroke="#657DA2"
+                        />
+                    </mask>
+                    <g mask="url(#maskCI0)">
+                        <path
+                            id="model_3_bg"
+                            d="M1441 422.822C1261.02 304.779 1071.42 188.888 873.882 76.9214C828.328 51.1014 782.858 25.7772 737.5 0.954684"
+                            stroke="#657DA2"
+                            stroke-opacity="0.4"
+                            stroke-width="2"
+                        />
+                        <path :id="`trail_${links[2].id}`" d="M512 980H179V592H512V980Z" fill="url(#paintCI2_radial)" />
+                    </g>
+                </g>
+                <g id="mask_2">
+                    <mask
+                        id="maskCI1"
+                        mask-type="alpha"
+                        maskUnits="userSpaceOnUse"
+                        x="221"
+                        y="0"
+                        width="1221"
+                        height="711"
+                    >
+                        <path
+                            :id="`model_${links[1].id}`"
+                            d="M1441 710.134C1236.58 564.525 1011.93 421.082 772.088 285.143C585.565 179.422 400.87 84.4813 221.257 0.999862"
+                            stroke="#657DA2"
+                        />
+                    </mask>
+                    <g mask="url(#maskCI1)">
+                        <path
+                            id="model_2_bg"
+                            d="M1441 710.134C1236.58 564.525 1011.93 421.082 772.088 285.143C585.565 179.422 400.87 84.4813 221.257 0.999862"
+                            stroke="#657DA2"
+                            stroke-opacity="0.4"
+                            stroke-width="2"
+                        />
+                        <path :id="`trail_${links[1].id}`" d="M512 980H179V592H512V980Z" fill="url(#paintCI0_radial)" />
+                    </g>
+                </g>
+                <g id="mask_1">
+                    <mask
+                        id="maskCI2"
+                        mask-type="alpha"
+                        maskUnits="userSpaceOnUse"
+                        x="0"
+                        y="173"
+                        width="1442"
+                        height="853"
+                    >
+                        <path
+                            :id="`model_${links[0].id}`"
+                            d="M1441,1024.77C1230.29,845.285 967.84,661.841 672.699,494.555C440.543,362.97 212.413,255.13 1,173.691"
+                            stroke="#657DA2"
+                        />
+                    </mask>
+                    <g mask="url(#maskCI2)">
+                        <path
+                            id="model_1_bg"
+                            d="M1 173.691C212.413 255.13 440.543 362.97 672.699 494.555C967.84 661.841 1230.29 845.285 1441 1024.77"
+                            stroke="#657DA2"
+                            stroke-opacity="0.4"
+                            stroke-width="2"
+                        />
+                        <path :id="`trail_${links[0].id}`" d="M419 861H86V473H419V861Z" fill="url(#paintCI1_radial)" />
+                    </g>
+                </g>
+                <g :id="`circle_wrapper_${links[0].id}`">
+                    <circle :id="`circle_${links[0].id}`" cx="681.5" cy="499.5" r="7.5" fill="#657DA2" />
+                </g>
+                <g :id="`border_wrapper_${links[0].id}`">
+                    <path
+                        :id="`border_${links[0].id}`"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M681.75 459.5C660.073 459.5 642.5 477.073 642.5 498.75C642.5 520.427 660.073 538 681.75 538C703.427 538 721 520.427 721 498.75C721 477.073 703.427 459.5 681.75 459.5ZM641 498.75C641 476.244 659.244 458 681.75 458C704.256 458 722.5 476.244 722.5 498.75C722.5 521.256 704.256 539.5 681.75 539.5C659.244 539.5 641 521.256 641 498.75Z"
+                        fill="#657DA2"
+                    />
+                </g>
+                <g :id="`circle_wrapper_${links[1].id}`">
+                    <circle :id="`circle_${links[1].id}`" cx="673.5" cy="228.5" r="7.5" fill="#F4F928" />
+                </g>
+                <g :id="`border_wrapper_${links[1].id}`">
+                    <path
+                        :id="`border_${links[1].id}`"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M673.75 188.5C652.073 188.5 634.5 206.073 634.5 227.75C634.5 249.427 652.073 267 673.75 267C695.427 267 713 249.427 713 227.75C713 206.073 695.427 188.5 673.75 188.5ZM633 227.75C633 205.244 651.244 187 673.75 187C696.256 187 714.5 205.244 714.5 227.75C714.5 250.256 696.256 268.5 673.75 268.5C651.244 268.5 633 250.256 633 227.75Z"
+                        fill="#F4F928"
+                    />
+                </g>
+                <g :id="`circle_wrapper_${links[2].id}`">
+                    <circle :id="`circle_${links[2].id}`" cx="965.5" cy="129.5" r="7.5" fill="#19232F" />
+                </g>
+                <g :id="`border_wrapper_${links[2].id}`">
+                    <path
+                        :id="`border_${links[2].id}`"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M965.75 89.5C944.073 89.5 926.5 107.073 926.5 128.75C926.5 150.427 944.073 168 965.75 168C987.427 168 1005 150.427 1005 128.75C1005 107.073 987.427 89.5 965.75 89.5ZM925 128.75C925 106.244 943.244 88 965.75 88C988.256 88 1006.5 106.244 1006.5 128.75C1006.5 151.256 988.256 169.5 965.75 169.5C943.244 169.5 925 151.256 925 128.75Z"
+                        fill="#19232F"
+                    />
+                </g>
+            </g>
+            <defs>
+                <radialGradient
+                    id="paintCI0_radial"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(512 786) rotate(-180) scale(333 194)"
+                >
+                    <stop stop-color="#F4F928" />
+                    <stop offset="1" stop-color="#F4F928" stop-opacity="0" />
+                </radialGradient>
+                <radialGradient
+                    id="paintCI1_radial"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(419 667) rotate(-180) scale(333 194)"
+                >
+                    <stop stop-color="#657DA2" />
+                    <stop offset="1" stop-color="#657DA2" stop-opacity="0" />
+                </radialGradient>
+                <radialGradient
+                    id="paintCI2_radial"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(419 667) rotate(-180) scale(333 194)"
+                >
+                    <stop stop-color="#19232F" />
+                    <stop offset="1" stop-color="#19232F" stop-opacity="0" />
+                </radialGradient>
+            </defs>
+        </svg>
+    </div>
+</template>
+<script>
+import { gsap } from 'gsap';
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
+gsap.registerPlugin(MotionPathPlugin);
+export default {
+    props: {
+        links: {
+            type: Array,
+            required: true
+        },
+        selected: {
+            type: String,
+            required: true
+        }
+    },
+    data: () => ({
+        tweens: []
+    }),
+    watch: {
+        selected(value, oldValue) {
+            if (value) {
+                this.slowMo(value);
+            } else {
+                this.resetSpeed(oldValue);
+            }
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            // this.setAppearInitialStyles();
+            this.initPlanets();
+            // this.appearAnimation();
+        });
+    },
+    methods: {
+        initPlanets() {
+            this.links.forEach((planet, index) => {
+                const star = [];
+                const id = planet.id;
+                gsap.set(`#border_${id}`, {
+                    scale: 0.25,
+                    opacity: 0
+                });
+                star[0] = gsap.to(`#circle_wrapper_${id}`, {
+                    motionPath: {
+                        path: `#model_${id}`,
+                        align: `#model_${id}`,
+                        alignOrigin: [0.5, 0.5],
+                        autoRotate: true,
+                        start: 0 + 0.3 * index,
+                        end: 1 + 0.3 * index
+                    },
+                    duration: 5,
+                    repeat: -1,
+                    ease: 'none'
+                });
+
+                star[1] = gsap.to(`#trail_${id}`, {
+                    motionPath: {
+                        path: `#model_${id}`,
+                        align: `#model_${id}`,
+                        alignOrigin: [1, 0.5],
+                        autoRotate: true,
+                        start: 0 + 0.3 * index,
+                        end: 1 + 0.3 * index
+                    },
+                    duration: 5,
+                    repeat: -1,
+                    ease: 'none'
+                });
+
+                star[2] = gsap.to(`#border_wrapper_${id}`, {
+                    motionPath: {
+                        path: `#model_${id}`,
+                        align: `#model_${id}`,
+                        alignOrigin: [0.5, 0.5],
+                        autoRotate: true,
+                        start: 0 + 0.3 * index,
+                        end: 1 + 0.3 * index
+                    },
+                    duration: 5,
+                    repeat: -1,
+                    ease: 'none'
+                });
+
+                this.tweens[id] = star;
+            });
+        },
+        slowMo(id) {
+            const planet = this.tweens[id];
+
+            gsap.killTweensOf([planet, `#border_${id}`, `#circle_${id}`]);
+            gsap.to(planet, {
+                duration: 1,
+                timeScale: 0.2
+            });
+            gsap.to(`#circle_${id}`, {
+                duration: 0.8,
+                scale: 2.6,
+                transformOrigin: '50% 50%'
+            });
+            gsap.to(`#border_${id}`, {
+                duration: 0.5,
+                scale: 1,
+                opacity: 1,
+                transformOrigin: '50% 50%',
+                delay: 0.2
+            });
+        },
+        resetSpeed(id, index) {
+            const planet = this.tweens[id];
+
+            gsap.killTweensOf([planet, `#border_${id}`, `#circle_${id}`]);
+            gsap.to(planet, {
+                duration: 1,
+                timeScale: 1
+            });
+            gsap.to(`#circle_${id}`, {
+                duration: 1,
+                scale: 1,
+                transformOrigin: '50% 50%'
+            });
+            gsap.to(`#border_${id}`, {
+                duration: 0.5,
+                scale: 0.25,
+                transformOrigin: '50% 50%',
+                opacity: 0
+            });
+        }
+    }
+};
+</script>

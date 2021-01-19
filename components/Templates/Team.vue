25dd445279d7aaef18ea47a27098d4467eb86517
menthe <adrien.le.mentheour@gmail.com>
2021-01-19 22:36:37 +0100

    💄 team lightmode

--- components/Templates/Team.vue
+++ components/Templates/Team.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="data">
+    <div v-if="data" class="wrapper-team">
         <div class="container">
             <div class="hero-team">
                 <h1 class="basic-h1" v-html="data.title"></h1>
@@ -14,11 +14,20 @@ export default {
     props: {
         data: { type: Object, required: true }
     },
-    computed: {}
+    head() {
+        return {
+            htmlAttrs: {
+                class: 'lightmode'
+            }
+        };
+    }
 };
 </script>
 
 <style lang="scss" scoped>
+.wrapper-team {
+    color: $orbit;
+}
 .hero-team {
     text-align: center;
     padding: 20px $gutter 40px;

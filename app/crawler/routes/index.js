11f12b190b00d737b6025744bb1b00ec5992eac8
Clément Lemoine <clementlemoine.cl@gmail.com>
2021-01-29 18:03:31 +0100

    ✨ portfolio link

--- app/crawler/routes/index.js
+++ app/crawler/routes/index.js
@@ -13,8 +13,12 @@ export const routes = {
         routerFormat: 'slug'
     },
     portfolio: {
-        i18nFormat: '_slug',
-        routerFormat: 'slug'
+        i18nFormat: 'portfolio/index',
+        routerFormat: 'portfolio'
+    },
+    portfolioSingle: {
+        i18nFormat: 'portfolio/_slug',
+        routerFormat: 'portfolio-slug'
     }
 };
 
@@ -22,7 +26,8 @@ export const routeByApiModels = {
     home: routes.home,
     page: routes.page,
     team: routes.team,
-    portfolio: routes.portfolio
+    portfolio: routes.portfolio,
+    company: routes.portfolioSingle
 };
 
 // You can pass an array of regular expressions to exclude dynamic routes.

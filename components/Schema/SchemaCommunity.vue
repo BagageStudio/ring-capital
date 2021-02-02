e7e00c12993c33c7fbb68b4ab106d97a4c0beb19
Clément Lemoine <clementlemoine.cl@gmail.com>
2021-02-02 17:15:39 +0100

    🚧 community schema

---
+++ components/Schema/SchemaCommunity.vue
@@ -0,0 +1,110 @@
+<template>
+    <div ref="schema" class="schema">
+        <svg
+            class="top-part"
+            width="784"
+            height="322"
+            viewBox="0 0 784 322"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g id="communityUp">
+                <g id="maskCom_1">
+                    <mask
+                        id="maskCom0"
+                        mask-type="alpha"
+                        maskUnits="userSpaceOnUse"
+                        x="351"
+                        y="-1"
+                        width="434"
+                        height="220"
+                    >
+                        <path
+                            id="modelCom_1"
+                            d="M351.986 0C492.588 63.7387 637.424 136.443 784.001 217.59"
+                            stroke="#657DA2"
+                        />
+                    </mask>
+                    <g mask="url(#maskCom0)">
+                        <path
+                            id="modelCom_1_2"
+                            opacity="0.4"
+                            d="M351.986 0C492.588 63.7387 637.424 136.443 784.001 217.59"
+                            stroke="#657DA2"
+                            stroke-width="2"
+                        />
+                        <path
+                            id="trailCom_1"
+                            d="M598.223 -87.0001L895.764 62.5264L721.541 409.211L424 259.685L598.223 -87.0001Z"
+                            fill="url(#paintCom0_radial)"
+                        />
+                    </g>
+                </g>
+                <g id="maskCom_2">
+                    <mask
+                        id="mask1"
+                        mask-type="alpha"
+                        maskUnits="userSpaceOnUse"
+                        x="-1"
+                        y="10"
+                        width="780"
+                        height="313"
+                    >
+                        <path
+                            id="modelCom_2"
+                            d="M0 10.7555C227.098 68.473 496.577 174.323 778 321.936"
+                            stroke="#657DA2"
+                        />
+                    </mask>
+                    <g mask="url(#mask1)">
+                        <path
+                            id="modelCom_2_2"
+                            opacity="0.4"
+                            d="M0 10.7555C227.098 68.473 496.577 174.323 778 321.936"
+                            stroke="#657DA2"
+                            stroke-width="2"
+                        />
+                        <path
+                            id="trailCom_2"
+                            d="M694.787 54L991.65 204.868L815.863 550.763L519 399.895L694.787 54Z"
+                            fill="url(#paintCom1_radial)"
+                        />
+                    </g>
+                </g>
+                <circle id="circle_1" cx="518.5" cy="77.5" r="7.5" fill="#657DA2" />
+                <circle id="circle_2" cx="605.5" cy="235.5" r="7.5" fill="#F4F928" />
+            </g>
+            <defs>
+                <radialGradient
+                    id="paintCom0_radial"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(511.111 86.3423) rotate(26.6814) scale(333 194)"
+                >
+                    <stop stop-color="#657DA2" />
+                    <stop offset="1" stop-color="#657DA2" stop-opacity="0" />
+                </radialGradient>
+                <radialGradient
+                    id="paintCom1_radial"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(606.894 226.947) rotate(26.9401) scale(333 194)"
+                >
+                    <stop stop-color="#F4F928" />
+                    <stop offset="1" stop-color="#F4F928" stop-opacity="0" />
+                </radialGradient>
+            </defs>
+        </svg>
+    </div>
+</template>
+<style lang="scss" scoped>
+.top-part {
+    position: absolute;
+    top: 0;
+    right: 0;
+}
+</style>

4eac70ab3bf7ef9d1f46dbeedc6c05a0d634567b
menthe <adrien.le.mentheour@gmail.com>
2021-02-16 10:46:24 +0100

    ✨ SE social digital

---
+++ components/SE/SocialDigital.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="vision-wrapper">
+        <div class="wrapper-section-1 container">
+            <div class="content-section-1">
+                <div class="wrapper-col-section left">
+                    <h3 v-if="data.section1LeftTitle" class="basic-h3 underlined">
+                        <span>{{ data.section1LeftTitle }}</span>
+                    </h3>
+                    <div
+                        v-if="data.section1LeftContent"
+                        class="basic-subtitle neptune"
+                        v-html="data.section1LeftContent"
+                    ></div>
+                </div>
+                <div class="wrapper-col-section right">
+                    <h3 v-if="data.section1RightTitle" class="basic-h3 underlined">
+                        <span>{{ data.section1RightTitle }}</span>
+                    </h3>
+                    <div
+                        v-if="data.section1RightContent"
+                        class="basic-subtitle neptune"
+                        v-html="data.section1RightContent"
+                    ></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        data: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.vision-wrapper {
+    overflow: hidden;
+    position: relative;
+    backface-visibility: hidden;
+    transform: translateZ(0);
+    will-change: transform;
+    transform-style: preserve-3d;
+}
+.wrapper-section-1 {
+    padding-top: 50px;
+    padding-bottom: 60px;
+}
+.content-section-1 {
+    padding: 0 $gutter;
+}
+
+@media (min-width: $tablet) {
+    .content-section-1 {
+        padding: 0;
+    }
+    .wrapper-col-section {
+        width: percentage(4/8);
+        padding: 0 $gutter;
+        backface-visibility: hidden;
+        transform: translateZ(0);
+        will-change: transform;
+        transform-style: preserve-3d;
+        &.left {
+            .basic-subtitle {
+                margin-bottom: 0;
+            }
+        }
+        &.right {
+            margin-left: auto;
+        }
+    }
+}
+@media (min-width: $desktop) {
+    .wrapper-section-1 {
+        padding-top: 190px;
+        padding-bottom: 190px;
+    }
+    .content-section-2 {
+        display: flex;
+        align-items: center;
+        padding: 0;
+    }
+    .wrapper-col-section {
+        width: percentage(5/12);
+        &.left {
+            order: 1;
+            padding-right: calc(#{percentage(1/12)} + #{$gutter});
+            margin-top: 110px;
+        }
+        &.right {
+            order: 3;
+            padding-left: calc(#{percentage(1/12)} + #{$gutter});
+            margin-top: -110px;
+        }
+    }
+}
+
+@media (min-width: $desktop-xxl) {
+    .wrapper-section-1 {
+        padding-top: 275px;
+        padding-bottom: 260px;
+    }
+    .wrapper-col-section {
+        &.left {
+            padding-left: calc(#{percentage(1/12)} + #{$gutter});
+            margin-top: 90px;
+        }
+        &.right {
+            padding-right: calc(#{percentage(1/12)} + #{$gutter});
+        }
+    }
+}
+</style>
--- components/Templates/SustainableEngagement.vue
+++ components/Templates/SustainableEngagement.vue
@@ -1,8 +1,17 @@
 <template>
-    <div v-if="data" class="wrapper-sustainableEngagement">
-        <div class="container">
-            <h1>Sustainable Engagement</h1>
+    <div v-if="data" class="wrapper-se">
+        <div class="hero-se">
+            <div class="container">
+                <div class="wrapper-title">
+                    <h1 class="basic-h1" v-html="$options.filters.nestedTitle(data.title, true)"></h1>
+                    <div v-if="data.subtitle || data.text" class="wrapper-subtitle">
+                        <p v-if="data.subtitle" class="basic-subtitle">{{ data.subtitle }}</p>
+                        <div v-if="data.text" class="hero-text neptune" v-html="data.text"></div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <SocialDigital :data="data" />
     </div>
 </template>
 
@@ -15,4 +24,27 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.hero-se {
+    text-align: center;
+    padding: 20px 0 60px;
+}
+.wrapper-title {
+    padding: 0 $gutter;
+}
+@media (min-width: $desktop-small) {
+    .hero-se {
+        padding: 90px 0 0;
+    }
+    .wrapper-subtitle {
+        max-width: 690px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .hero-text {
+        max-width: 430px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+}
+</style>

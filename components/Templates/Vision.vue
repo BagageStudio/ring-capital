abab7de83cf9a80b8c3e9c2e7fb80b6d5242de5b
menthe <adrien.le.mentheour@gmail.com>
2021-02-22 12:06:08 +0100

    ✨ believe

--- components/Templates/Vision.vue
+++ components/Templates/Vision.vue
@@ -3,14 +3,12 @@
         <div class="hero-vision">
             <div class="container">
                 <div class="wrapper-title">
-                    <h1 class="basic-h1" v-html="$options.filters.nestedTitle(data.title, true)"></h1>
-                    <!-- <div v-if="data.subtitle || data.text" class="wrapper-subtitle">
-                        <p v-if="data.subtitle" class="basic-subtitle">{{ data.subtitle }}</p>
-                        <div v-if="data.text" class="hero-text neptune" v-html="data.text"></div>
-                    </div> -->
+                    <h1 class="basic-h1 strong-saturn" v-html="$options.filters.nestedTitle(data.title, true)"></h1>
+                    <p v-if="data.subtitle" class="basic-subtitle">{{ data.subtitle }}</p>
                 </div>
             </div>
         </div>
+        <Believe :content="data" />
     </div>
 </template>
 

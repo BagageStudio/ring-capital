3bdaae1cfd7247e77e734f7766d699d2ed09a998
Clément Lemoine <clementlemoine.cl@gmail.com>
2024-07-25 18:42:34 +0200

    ✨ slider video home

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,25 +1,38 @@
 <template>
     <div class="hero">
-        <video class="hero-video" playsinline muted loop autoplay>
-            <source :src="data.heroSlides[0].video.url" :type="`video/${data.heroSlides[0].video.format}`" />
-        </video>
+        <div class="videos">
+            <video
+                v-for="slide in data.heroSlides"
+                :key="slide.id"
+                ref="videos"
+                class="hero-video"
+                playsinline
+                muted
+                loop
+            >
+                <source :src="slide.video.url" :type="`video/${slide.video.format}`" />
+            </video>
+        </div>
         <div class="container-hero container">
             <div class="wrapper-txt">
                 <h1 class="hero-title" v-html="$options.filters.noPAround(data.title)"></h1>
-                <div class="subtitles">
-                    <div
-                        v-for="slide in data.heroSlides"
-                        :key="slide.id"
-                        class="subtitle"
-                        v-html="$options.filters.noPAround(slide.title)"
-                    ></div>
+                <div class="subtitles-wrapper">
+                    <div class="subtitles">
+                        <div
+                            v-for="slide in data.heroSlides"
+                            :key="slide.id"
+                            ref="subtitles"
+                            class="subtitle"
+                            v-html="$options.filters.noPAround(slide.title)"
+                        ></div>
+                    </div>
                 </div>
             </div>
             <div class="wrapper-arrows">
-                <button class="arrow" type="button">
+                <button class="arrow" type="button" @click="prevSlide">
                     <Icon name="prev" />
                 </button>
-                <button class="arrow" type="button">
+                <button class="arrow" type="button" @click="nextSlide">
                     <Icon name="next" />
                 </button>
             </div>
@@ -39,8 +52,120 @@ export default {
     },
     data() {
         return {
-            currentSlide: 0
+            currentSlide: -1,
+            timer: null,
+            transitionning: false
         };
+    },
+    beforeDestroy() {
+        this.killAnim();
+    },
+    mounted() {
+        this.changeSlide(0);
+    },
+    methods: {
+        prevSlide() {
+            const prevIndex = this.currentSlide === 0 ? this.data.heroSlides.length - 1 : this.currentSlide - 1;
+            this.changeSlide(prevIndex);
+        },
+        nextSlide() {
+            const nextIndex = this.currentSlide === this.data.heroSlides.length - 1 ? 0 : this.currentSlide + 1;
+            this.changeSlide(nextIndex);
+        },
+        triggerAutoTimer() {
+            if (this.timer) this.timer.kill();
+            this.timer = this.$gsap.delayedCall(4, () => {
+                this.nextSlide();
+            });
+        },
+        changeSlide(nextIndex) {
+            if (this.transitionning || nextIndex === this.currentSlide) return;
+
+            this.transitionning = true;
+
+            const currentVideo = this.$refs.videos[this.currentSlide];
+            const nextVideo = this.$refs.videos[nextIndex];
+
+            const currentSubtitle = this.$refs.subtitles[this.currentSlide];
+            const nextSubtitle = this.$refs.subtitles[nextIndex];
+
+            nextVideo.play();
+
+            // Disappear
+            this.$gsap.set(currentVideo, {
+                zIndex: 1
+            });
+            if (currentVideo) {
+                this.$gsap.to(currentVideo, {
+                    duration: 0.7,
+                    scale: 1.4,
+                    ease: 'power2.inOut',
+                    force3D: true,
+                    overwrite: true
+                });
+            }
+            if (currentSubtitle) {
+                this.$gsap.to(currentSubtitle, {
+                    duration: 0.7,
+                    autoAlpha: 0,
+                    ease: 'power2.inOut',
+                    overwrite: true
+                });
+            }
+
+            // Reappear
+            this.$gsap.set(nextVideo, {
+                zIndex: 2
+            });
+            this.$gsap.fromTo(
+                nextVideo,
+                {
+                    autoAlpha: 0,
+                    scale: 1.01
+                },
+                {
+                    duration: 0.7,
+                    autoAlpha: 1,
+                    scale: 1.1,
+                    force3D: true,
+                    ease: 'power2.inOut',
+                    overwrite: true,
+                    onComplete: () => {
+                        if (currentVideo) {
+                            currentVideo.pause();
+                            currentVideo.currentTime = 0;
+                            this.$gsap.set(currentVideo, {
+                                zIndex: 0,
+                                autoAlpha: 0
+                            });
+                        }
+                        this.transitionning = false;
+                        this.triggerAutoTimer(nextIndex);
+                        this.currentSlide = nextIndex;
+                    }
+                }
+            );
+            this.$gsap.fromTo(
+                nextSubtitle,
+                {
+                    autoAlpha: 0
+                },
+                {
+                    duration: 0.7,
+                    autoAlpha: 1,
+                    ease: 'power2.inOut',
+                    delay: 0.1,
+                    overwrite: true
+                }
+            );
+        },
+        killAnim() {
+            this.currentSlide = 0;
+            if (this.timer) this.timer.kill();
+            this.timer = null;
+            this.$gsap.set(this.$refs.videos, { clearProps: 'all' });
+            this.$gsap.set(this.$refs.subtitles, { clearProps: 'all' });
+        }
     }
 };
 </script>
@@ -48,6 +173,15 @@ export default {
 <style lang="scss" scoped>
 .hero {
     position: relative;
+}
+.videos {
+    position: absolute;
+    top: -1px;
+    left: -1px;
+    right: -1px;
+    bottom: -1px;
+    overflow: hidden;
+    z-index: 1;
 }
 .hero-video {
     display: block;
@@ -57,6 +191,9 @@ export default {
     width: 100%;
     height: 100%;
     object-fit: cover;
+    opacity: 0;
+    will-change: transform;
+    z-index: 0;
 }
 .container-hero {
     position: relative;
@@ -67,9 +204,7 @@ export default {
     padding-top: calc(var(--header-height) + 8rem);
     padding-bottom: 8rem;
     color: var(--bg);
-    &::before {
-        content: '';
-    }
+    z-index: 2;
 }
 .wrapper-txt {
     width: 100%;
@@ -81,15 +216,24 @@ export default {
     font-weight: 600;
     padding: 0 var(--gutter);
 }
-.subtitles {
-    margin-top: 6rem;
+.subtitles-wrapper {
     padding: 0 var(--gutter);
+    margin-top: 6rem;
+}
+.subtitles {
+    position: relative;
+    height: 100%;
 }
 .subtitle {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
     font-family: var(--urbanist);
     font-size: 3.5rem;
     line-height: 3.5rem;
     font-weight: 600;
+    opacity: 0;
 }
 .wrapper-arrows {
     display: flex;
@@ -112,23 +256,30 @@ export default {
 
 @media (min-width: $desktop) {
     .container-hero {
+        justify-content: center;
         padding-top: 10rem;
         padding-bottom: 10rem;
     }
     .wrapper-txt {
         display: flex;
-        align-items: flex-end;
+    }
+    .subtitle {
+        top: auto;
+        bottom: 0;
     }
     .hero-title {
         flex: 0 0 auto;
         width: calc(7 / 12 * 100%);
     }
-    .subtitles {
+    .subtitles-wrapper {
         flex: 0 0 auto;
         width: calc(5 / 12 * 100%);
         margin-top: 0;
     }
     .wrapper-arrows {
+        position: absolute;
+        right: var(--gutter);
+        bottom: 10rem;
         justify-content: flex-end;
     }
 }

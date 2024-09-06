fa6b4f428ebae70b13b314df5d90943821c8965a
Clément Lemoine <clementlemoine.cl@gmail.com>
2024-09-06 11:47:14 +0200

    ✨ favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,25 +38,22 @@ export default {
         ],
         link: [
             {
+                rel: 'icon',
+                href: '/favicon.ico',
+                sizes: 'any'
+            },
+            {
+                rel: 'icon',
+                href: '/icon.svg',
+                type: 'image/svg+xml'
+            },
+            {
                 rel: 'apple-touch-icon',
-                sizes: '180x180',
                 href: '/apple-touch-icon.png'
             },
             {
-                rel: 'icon',
-                sizes: '32x32',
-                href: '/favicon-32x32.png'
-            },
-            {
-                rel: 'icon',
-                type: 'image/png',
-                sizes: '16x16',
-                href: '/favicon-16x16.png'
-            },
-            {
-                rel: 'mask-icon',
-                href: '/safari-pinned-tab.svg',
-                color: '#5bbad5'
+                rel: 'manifest',
+                href: '/manifest.webmanifest'
             },
             {
                 rel: 'stylesheet',
